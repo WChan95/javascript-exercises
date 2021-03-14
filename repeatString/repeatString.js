0ede69d59f705a2cec46abb597bb2e17ceb3caa9
Soejun <wc.wilsonch@gmail.com>
2021-03-14 16:57:48 -0700

    Completed repeatString exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(stringRepeat, numRepeat) {
 
+        let i;
+        let stringFinal = "";
+
+        for(i = 0; i < numRepeat; i++){
+            stringFinal = stringFinal + stringRepeat;
+        }
+
+        return stringFinal;
 }
 
 module.exports = repeatString
