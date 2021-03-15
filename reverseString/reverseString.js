61fffe61efac539c6dd9261ff93126abba814222
Soejun <wc.wilsonch@gmail.com>
2021-03-15 02:19:56 -0700

    Completed all test cases

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
 
+    let stringSplit = [];
+    let stringReversed = "";
+    let i;
+
+    for(i = 0; i < stringToReverse.length; i++){
+        stringSplit.push(stringToReverse.charAt(stringToReverse.length - i - 1))
+    }
+
+    for(i = 0; i < stringToReverse.length; i++){
+        stringReversed = stringReversed + stringSplit[i];
+    }
+
+    return stringReversed;
 }
 
 module.exports = reverseString
