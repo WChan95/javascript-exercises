fa6af3c435cb7d87774b8e25797f36c589ef68bb
Soejun <wc.wilsonch@gmail.com>
2021-03-21 21:55:32 -0700

    Completed all test cases

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
+    if(inputYear % 4 == 0 && inputYear % 100 != 0){
+        return true;
+    }else if(inputYear % 100 == 0){
+        if(inputYear % 400 == 0){
+            return true;
+        }else{
+            return false;
+        }
 
+    }else{
+        return false;
+    }
 }
 
 module.exports = leapYears
