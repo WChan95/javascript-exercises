943bc5ebffec437f2080eb0e60d2772eb332a72f
Soejun <wc.wilsonch@gmail.com>
2021-04-21 00:34:12 -0700

    completed all test cases

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -7,19 +7,31 @@ function subtract (x, y) {
 }
 
 function sum (array){
-	return array.reduce(total, sum);
+	const sum = array.reduce((accumulator,currentValue) => {
+		return accumulator + currentValue;}, 0);
+	return sum;
 }
 
-function multiply () {
-	
+function multiply (array) {
+	const total = array.reduce((accumulator, currentValue) =>{
+		return accumulator * currentValue;}, 1);
+	return total;
+	}
+
+function power(a, b) {
+	return Math.pow(a, b);
 }
 
-function power() {
-	
-}
-
-function factorial() {
-	
+function factorial(x) {
+	//x = 10, while x > 0, x--, multiply
+	let accumulator = 1;
+	let currentValue;
+	let i;
+	for(i = x; x > 0; x--){
+		currentValue = x;
+		accumulator = accumulator * currentValue;
+	}
+	return accumulator;
 }
 
 module.exports = {
