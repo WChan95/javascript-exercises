e36fccba55f3c1a68b79c24427673eb6e0a66a96
Soejun <wc.wilsonch@gmail.com>
2021-04-21 00:37:10 -0700

    Brainstorming

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,7 @@
 const palindromes = function() {
-
+    //  - A car, a man, a maraca.
+    //Remove all white spaces, commas, apostrophes
+    //return string after above removed
 }
 
 module.exports = palindromes
