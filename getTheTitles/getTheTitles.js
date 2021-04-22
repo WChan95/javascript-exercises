68b0a45f2bb0e3b045d520e842f4245d5c903dba
Soejun <wc.wilsonch@gmail.com>
2021-04-21 23:00:47 -0700

    solved

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let justTitles = books.map(book => book.title);
+    return justTitles;
 }
 
 module.exports = getTheTitles;
