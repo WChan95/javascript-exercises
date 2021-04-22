4a961e3980e15bc171a3b702e4fca1be5cba12a4
Soejun <wc.wilsonch@gmail.com>
2021-04-21 22:35:12 -0700

    Solved

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -2,9 +2,26 @@ const palindromes = function(palindrome) {
     //  - A car, a man, a maraca.
     //Remove all white spaces, commas, apostrophes
     //return string after above removed
-    palindrome = palindrome.replace(/[^\w\s]|_/g, "")
-                            .replace(/\s/g, "");
+    let palindromeInitial = palindrome.toLowerCase()
+                                        .replace(/[^\w\s]|_/g, "")
+                                        .replace(/\s/g, "")
+                                        .split("");
 
+    let palindromeReverse = [];
+    let i;
+
+    for(i = palindromeInitial.length - 1; i >= 0; i--){
+        palindromeReverse.push(palindromeInitial[i]);
+    }
+
+    let isPalindrome = true;
+    for(i = 0; i < palindromeInitial.length; i++){
+        if(palindromeInitial[i] != palindromeReverse[i]){
+            isPalindrome = false;
+            break;
+        }
+    }
+    return isPalindrome;
 }
 
 module.exports = palindromes
