6493e6b56b989bd7d02b049814f60ea006c32cee
Soejun <wc.wilsonch@gmail.com>
2021-04-21 20:55:29 -0700

    remove punctuations and whitespace from string

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,7 +1,10 @@
-const palindromes = function() {
+const palindromes = function(palindrome) {
     //  - A car, a man, a maraca.
     //Remove all white spaces, commas, apostrophes
     //return string after above removed
+    palindrome = palindrome.replace(/[^\w\s]|_/g, "")
+                            .replace(/\s/g, "");
+
 }
 
 module.exports = palindromes
