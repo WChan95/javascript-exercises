6a952cdbc1b619025907f9e9bfcf1d69a4bbe99a
Soejun <wc.wilsonch@gmail.com>
2021-04-23 09:10:47 -0700

    test case 1

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(persons) {
+    let i;
+    let atAge = 0;
+    let currentAge = 0;
+    let oldestPerson = {};
+    for(i = 0; i < persons.length; i++){
+        let nextAge = persons[i].yearOfDeath - persons[i].yearOfBirth;
+        if(currentAge > nextAge){
+            currentAge = currentAge;
+            atAge = atAge;
+        }else{
+            currentAge = nextAge;
+            atAge = i;
+        }
+    }
+    oldestPerson = persons[atAge];
+    return oldestPerson;
 }
 
 module.exports = findTheOldest
