9ebb9594e205c74321cf96e9a71b88bf0c922160
Soejun <wc.wilsonch@gmail.com>
2021-04-24 20:56:24 -0700

    solved

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
 const findTheOldest = function(array) {
     
+
+  return array.reduce(function(oldest,currentPerson){
+    const oldestLived = yearsLived(oldest.yearOfDeath,oldest.yearOfBirth);
+    const currentPersonLived = yearsLived(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
+
+    if(oldestLived > currentPersonLived){
+      oldest = oldest;
+    }else{
+      oldest = currentPerson;
+    }
+    return oldest;
+  });
+
   }
-  
+
+  const yearsLived = (death, birth) =>{
+    let d = new Date();
+    if(!death){
+      death = new Date().getFullYear();
+    }
+      return death - birth;
+  }
   module.exports = findTheOldest
   
